086d0eb843d4480b7aa48e46730e0d79a6da4a31
Georg Wechslberger <wechslbe@ma.tum.de>
2018-12-16 00:40:38 +0100

    added handling of NDEF tag messages

--- android/src/main/kotlin/it/matteocrippa/flutternfcreader/FlutterNfcReaderPlugin.kt
+++ android/src/main/kotlin/it/matteocrippa/flutternfcreader/FlutterNfcReaderPlugin.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.nfc.NfcAdapter
 import android.nfc.NfcManager
 import android.nfc.Tag
+import android.nfc.tech.Ndef
 import android.os.Build
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.nio.charset.Charset
 
 class FlutterNfcReaderPlugin(val registrar: Registrar) : MethodCallHandler,  NfcAdapter.ReaderCallback {
     private var isReading = false
@@ -84,7 +86,18 @@ class FlutterNfcReaderPlugin(val registrar: Registrar) : MethodCallHandler,  Nfc
 
     // handle discovered NDEF Tags
     override fun onTagDiscovered(tag: Tag?) {
-        resulter?.success(bytesToHexString(it.id))
+        // convert tag to NDEF tag
+        val ndef = Ndef.get(tag)
+        // ndef will be null if the discovered tag is not a NDEF tag
+        // read NDEF message
+        ndef?.connect()
+        val message = ndef?.getNdefMessage()
+                          ?.toByteArray()
+                          ?.toString(Charset.forName("UTF-8"))
+        ndef?.close()
+        if (message != null) {
+            resulter?.success(message)
+        }
     }
 
     private fun bytesToHexString(src: ByteArray?): String? {
