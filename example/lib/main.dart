951dca867b551eb6b92b43b56312d0c6d25f0a73
matteocrippa <matteocrippa@users.noreply.github.com>
2019-09-29 18:42:57 +0200

    minor change

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextEditingController writerController = TextEditingController();
-    writerController.text = "prueba";
+    writerController.text = "Test NFC";
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
