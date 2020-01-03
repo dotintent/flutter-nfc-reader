da3bf29319c50c1f1ce7292803b9a6fed38a9133
Michael Rittmeister <michael@rittmeister.in>
2020-01-03 12:04:48 +0100

    Upgrade android project - Upgrade Kotlin to 1.3.61 - Upgrade Android plugin 3.3.1 - Change minSdk to 19 since it's the first NFC supporting release

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -55,7 +55,7 @@ class _NfcScanState extends State<NfcScan> {
         ),
         RaisedButton(
           onPressed: () {
-            FlutterNfcReader.read();
+            FlutterNfcReader.read(instruction: "It's reading");
           },
           child: Text("Read"),
         ),
